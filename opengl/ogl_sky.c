df8d4e698d35eac3a1fad1d8f6e50d4f4cadddcb
Ozkan Sezer <sezeroz@gmail.com>
2009-05-21 09:50:15 +0000

    heretic opengl: massive whitespace/coding style cleanup. byte order fixes.  other syncing with hhexen.  also removed the dead ogl_tex.h header.
    git-svn-id: svn://svn.code.sf.net/p/hhexen/code/trunk/hheretic@397 989aa981-fe4f-0410-aa6a-c19d413bcdcd

--- opengl/ogl_sky.c
+++ opengl/ogl_sky.c
@@ -11,6 +11,12 @@
 // HEADER FILES ------------------------------------------------------------
 
 #include "h2stdinc.h"
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
 #include <math.h>
 #include <GL/gl.h>
 #include "doomdef.h"
@@ -32,54 +38,60 @@ extern float		texw, texh;
 
 extern float		vx, vy, vz;
 
-extern byte			topLineRGB[3];
-int			skyDetail = 1;
+extern byte		topLineRGB[3];
 
 // PUBLIC DATA DEFINITIONS -------------------------------------------------
 
+int			skyDetail = 1;
 int			skyhemispheres;
-fadeout_t	fadeOut[2];		// For both skies.
+fadeout_t		fadeOut[2];	/* For both skies. */
 
 // PRIVATE DATA DEFINITIONS ------------------------------------------------
 
-// CODE --------------------------------------------------------------------
-
-// The texture offset to be applied to the texture coordinates in SkyVertex().
+/* The texture offset to be applied to the texture coordinates in SkyVertex(). */
 static float		maxSideAngle = (float) PI/3;
 static float		texoff;
-static int			rows, columns;	
-static float		scale = 32;	// Fogging affects, thus close-by.
+static int		rows, columns;
+static float		scale = 32;	/* Fogging affects, thus close-by. */
 static boolean		yflip;
 static fadeout_t	*currentFO;
+
+// CODE --------------------------------------------------------------------
 
 // Calculate the vertex and texture coordinates.
 static void SkyVertex(int r, int c)
 {
 	// The direction must be clockwise.
 	float topAngle = c/(float)columns * 2*PI;
-	float sideAngle = maxSideAngle * (rows-r)/(float)rows;
+	float sideAngle = maxSideAngle * (rows - r)/(float)rows;
 	float height = sin(sideAngle);
 	float realRadius = scale*cos(sideAngle);
 	float x = vx + realRadius*cos(topAngle), 
-			z = vz + realRadius*sin(topAngle),
-			y = vy + ((!yflip)? scale*height : -scale*height);	
+		z = vz + realRadius*sin(topAngle),
+		y = vy + ((!yflip) ? scale*height : -scale*height);
 
 	// And the texture coordinates.
-	if(!yflip)	// Flipped Y is for the lower hemisphere.
+	if (!yflip)	// Flipped Y is for the lower hemisphere.
 		glTexCoord2f(4*c/(float)columns + texoff/texw, r/(float)rows*200.0/256.0);
 	else
 		glTexCoord2f(4*c/(float)columns + texoff/texw, (rows-r)/(float)rows*200.0/256.0);
 	// Also the color.
-	if(currentFO->use)
+	if (currentFO->use)
 	{
-		if(r==0) glColor4f(1,1,1,0); else glColor3f(1,1,1);
+		if (r == 0)
+			glColor4f(1, 1, 1, 0);
+		else
+			glColor3f(1, 1, 1);
 	}
 	else
 	{
-		if(r==0) glColor3f(0,0,0); else glColor3f(1,1,1);
+		if (r == 0)
+			glColor3f(0, 0, 0);
+		else
+			glColor3f(1, 1, 1);
 	}
 	// And finally the vertex.
-	glVertex3f(x, y, z);	
+	glVertex3f(x, y, z);
 }
 
 static void CapSideVertex(int r, int c)
@@ -91,8 +103,8 @@ static void CapSideVertex(int r, int c)
 	float realRadius = scale*cos(sideAngle);
 
 	glVertex3f(vx + realRadius*cos(topAngle),
-		vy + ((!yflip)? scale*height : -scale*height), 
-		vz + realRadius*sin(topAngle));	
+		   vy + ((!yflip) ? scale*height : -scale*height),
+		   vz + realRadius*sin(topAngle));
 }
 
 // Hemi is Upper or Lower. Zero is not acceptable.
@@ -101,7 +113,10 @@ void OGL_RenderSkyHemisphere(int hemi)
 {
 	int		r, c;
 
-	if(hemi & SKYHEMI_LOWER) yflip = true; else yflip = false;
+	if (hemi & SKYHEMI_LOWER)
+		yflip = true;
+	else
+		yflip = false;
 
 	// The top row (row 0) is the one that's faded out.
 	// There must be at least 4 columns. The preferable number
@@ -109,28 +124,28 @@ void OGL_RenderSkyHemisphere(int hemi)
 	// two rows because the first one is always faded.
 	rows = 3;
 	columns = 4*skyDetail;	// 4n
-	if(hemi & SKYHEMI_JUST_CAP)
+	if (hemi & SKYHEMI_JUST_CAP)
 	{
-		//glBindTexture(GL_TEXTURE_2D, 0);
-		glDisable( GL_TEXTURE_2D );
+		glDisable(GL_TEXTURE_2D);
 
 		// Use the appropriate color.
-		if(currentFO->use)
+		if (currentFO->use)
 			glColor3fv(currentFO->rgb);
 		else
-			glColor3f(0,0,0);
+			glColor3f(0, 0, 0);
 		glBegin(GL_TRIANGLE_FAN);
-		for(c=0; c<columns; c++) CapSideVertex(0, c);
+		for (c = 0; c < columns; c++)
+			CapSideVertex(0, c);
 		glEnd();
 
 		// If we are doing a colored fadeout...
-		if(currentFO->use)
+		if (currentFO->use)
 		{
 			// We must fill the background for the top row since it'll
 			// be partially translucent.
 			glBegin(GL_TRIANGLE_STRIP);
 			CapSideVertex(0, 0);
-			for(c=0; c<columns; c++)
+			for (c = 0; c < columns; c++)
 			{
 				CapSideVertex(1, c);	// One step down.
 				CapSideVertex(0, c+1);	// And one step right.
@@ -139,18 +154,18 @@ void OGL_RenderSkyHemisphere(int hemi)
 			glEnd();
 		}
 
-		glEnable( GL_TEXTURE_2D );
+		glEnable(GL_TEXTURE_2D);
 		return;
 	}
 
 	// The total number of triangles per hemisphere can be calculated
 	// as follows: rows * columns * 2 + 2 (for the top cap).
-	for(r=0; r<rows; r++)
+	for (r = 0; r < rows; r++)
 	{
 		glBegin(GL_TRIANGLE_STRIP);
 		// Add the start vertex.
 		SkyVertex(r, 0);
-		for(c=0; c<columns; c++)
+		for (c = 0; c < columns; c++)
 		{
 			SkyVertex(r+1, c);	// One step down.
 			SkyVertex(r, c+1);	// And one step right.
@@ -159,15 +174,17 @@ void OGL_RenderSkyHemisphere(int hemi)
 		// the start vertex for the next row.
 		SkyVertex(r+1, c);
 		glEnd();
-		/*glBegin(GL_QUADS);
-		for(c=0; c<columns; c++)
+		/*
+		glBegin(GL_QUADS);
+		for (c = 0; c < columns; c++)
 		{
 			SkyVertex(r, c);
 			SkyVertex(r+1, c);
 			SkyVertex(r+1, c+1);
 			SkyVertex(r, c+1);
 		}
-		glEnd();*/
+		glEnd();
+		*/
 	}
 }
 
@@ -175,20 +192,23 @@ void OGL_HandleColoredFadeOut(int skynum)
 {
 	int			i;
 
-	currentFO = fadeOut + skynum-1;
+	currentFO = fadeOut + skynum - 1;
 	// We have to remember the top line average RGB.
-	if(!currentFO->set)
+	if (!currentFO->set)
 	{
 		currentFO->set = 1;	// Now it is.
-		for(i=0; i<3; i++) currentFO->rgb[i] = topLineRGB[i]/255.0;
+		for (i = 0; i < 3; i++)
+			currentFO->rgb[i] = topLineRGB[i]/255.0;
 		// Determine if it should be used.
-		for(currentFO->use=false, i=0; i<3; i++)
-			if(currentFO->rgb[i] > .3)
+		for (currentFO->use = false, i = 0; i < 3; i++)
+		{
+			if (currentFO->rgb[i] > .3)
 			{
 				// Colored fadeout is needed.
 				currentFO->use = true;
 				break;
 			}
+		}
 	}
 }
 
@@ -211,7 +231,8 @@ static void OGL_DrawSkyhemi(int whichHemi)
 void R_RenderSkyHemispheres(int hemis)
 {
 	// IS there a sky to be rendered?
-	if(!hemis) return;
+	if (!hemis)
+		return;
 
 	// We don't want anything written in the depth buffer, not yet.
 	glDepthMask(GL_FALSE);
@@ -220,9 +241,12 @@ void R_RenderSkyHemispheres(int hemis)
 	glDisable(GL_CULL_FACE);
 	glDisable(GL_FOG);
 	// Draw the possibly visible hemispheres.
-	if(hemis & SKYHEMI_UPPER) OGL_DrawSkyhemi(SKYHEMI_UPPER);
-	if(hemis & SKYHEMI_LOWER) OGL_DrawSkyhemi(SKYHEMI_LOWER);
+	if (hemis & SKYHEMI_UPPER)
+		OGL_DrawSkyhemi(SKYHEMI_UPPER);
+	if (hemis & SKYHEMI_LOWER)
+		OGL_DrawSkyhemi(SKYHEMI_LOWER);
 	// Enable the disabled things.
 	glPopAttrib();
 	glDepthMask(GL_TRUE);
 }
+
